56953bcca829e2043abad9a04a366a98a9ee7b22
Behruz Gurbanli <behruzgurbanli@gmail.com>
2024-01-01 20:55:17 +0400

    updated the structure

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App.js';
+import App from './App';
+import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-    <App />
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
