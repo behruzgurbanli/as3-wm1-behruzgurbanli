a6b8e443a9e08a7ae832a6ef3dd392bebd4bffb2
Behruz Gurbanli <behruzgurbanli@gmail.com>
2024-01-01 20:50:08 +0400

    updated the structure

--- src/App.js
+++ src/App.js
@@ -1,27 +1,7 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 
 function App() {
-    const [flashCards, setFlashCards] = useState([]);
+
+}
   
-    useEffect(() => {
-      fetch('http://localhost:3001/flashCards')
-        .then(response => response.json())
-        .then(data => setFlashCards(data));
-    }, []);
-  
-    return (
-      <div>
-        <h1>Flash Card App</h1>
-        <ul>
-          {flashCards.map(card => (
-            <li key={card.id}>
-              <p>Question:</p> {card.question}<br />
-              <p>Answer:</p> {card.answer}
-            </li>
-          ))}
-        </ul>
-      </div>
-    );
-  }
-  
-  export default App;
+export default App;
