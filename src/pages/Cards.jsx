0fe94ddb4679f183ffd54d83ce4342ef720cd5fc
Behruz Gurbanli <behruzgurbanli@gmail.com>
2024-01-02 00:44:21 +0400

    FlashCards page started

---
+++ src/pages/Cards.jsx
@@ -0,0 +1,20 @@
+import React, { useState, useEffect, useCallback } from 'react';
+import axios from 'axios';
+import '../assets/cards.css'
+
+function Cards() {
+    const [cards, setCards] = useState([]);
+
+    const fetchCards = useCallback(async () => {
+        try {
+            const response = await axios.get("http://localhost:3001/cards?_limit=10&");
+            const initialFetch = response.data;
+
+            setCards(initialFetch);
+        } catch (error) {
+            console.error("Error: ", error);
+        }
+    }, []);
+}
+
+export default Cards;
