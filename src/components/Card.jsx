f484fd537a348b697e71073596b1306689223ab8
Behruz Gurbanli <behruzgurbanli@gmail.com>
2024-01-02 22:44:30 +0400

    flipping functionality added

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,28 @@
 import React, { useState } from 'react';
-import '../assets/flashcard.css';
+import '../assets/card.css';
 
 const Card = (props) => {
     const [isFlipped, setIsFlipped] = useState(false);
 
+    const handleFlip = () => {
+        setIsFlipped(!isFlipped);
+    };
+
     return (
         
-        <div className="flashcard">
-            <h4> <strong> { props.id }. </strong> { props.front }</h4>
-            <p>Answer: { props.back }</p>
-            <p>Status: { props.status }</p>
-            <p>Last Modified: { new Date(props.lastModified).toLocaleDateString() }</p>
+        <div className={`card ${isFlipped ? 'flipped' : ''}`} onClick={handleFlip}>
+        <div className="card-inner">
+            <div className="card-front">
+                <h4> <strong> { props.id }. </strong> { props.front }</h4>
+                <p>Status: { props.status }</p>
+                <p>Last Modified: { new Date(props.lastModified).toLocaleDateString() }</p>
+            </div>
+            <div className="card-back">
+                <p>Answer: { props.back }</p>
+            </div>
             <button onClick={() => props.onDelete(props.id)}>Delete</button>
         </div>
+    </div>
         
     );
 }
